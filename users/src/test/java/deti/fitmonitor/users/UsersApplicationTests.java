94d4c59aa3bedadabf80430944db01260e9911fd
DiogoSilva1904 <diogompsilva2000@gmail.com>
2024-10-30 17:18:14 +0000

    tests update

--- users/src/test/java/deti/fitmonitor/users/UsersApplicationTests.java
+++ users/src/test/java/deti/fitmonitor/users/UsersApplicationTests.java
@@ -1,13 +1,8 @@
 package deti.fitmonitor.users;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UsersApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
